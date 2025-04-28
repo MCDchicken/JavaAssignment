b78b1f9a22b9f0d3aaea7ebda2dfed3b5265af7e
winwin <jhieying@gmail.com>
2025-04-29 00:28:28 +0800

    Update FeedbackSupport.java

--- FeedbackSupport.java
+++ FeedbackSupport.java
@@ -7,48 +7,73 @@ public class FeedbackSupport {
     Scanner sc = new Scanner(System.in);
 
     // instance variable
-    private int breakfastPax;
-    public static final double BREAKFASTPRICE = 30.0;
-
+    private int complaintType;
+    private String complaintText;
 
     // No-arg Constructor
     public FeedbackSupport() {
-        breakfastPax = 0;
-    }
-
-    // Parameterized Constructor
-    public FeedbackSupport(int breakfastPax) {
-        this.breakfastPax = breakfastPax;
+        complaintType = 0;
+        complaintText = "";
     }
 
     // Getter
-    public int getBreakfastPax() {
-        return breakfastPax;
+    public int getComPlaintType() {
+        return complaintType;
     }
 
-    // Setter
-    public void setBreakfastPax(int breakfastPax) {
-        this.breakfastPax = breakfastPax;
-    }
+    // no need setter because prompt from user
 
     // Methods
-    public void promptComplaint() { //call this when run
+    public void promptComplaintStaff() { //call this when run
+        System.out.println();
+        System.out.println("====================================");
+        System.out.println("|                                  |");
+        System.out.println("|         FEEDBACK REPORT          |");
+        System.out.println("|                                  |");
+        System.out.println("====================================");
+
+        do{
+            System.out.println("Select complaint type: " + "\n1. Room Issue\n2. Facilities\n3. House keeping\n4. Billing\n5. Food\n6. Service\n\n0. Exit");
+            System.out.print("Enter your choice: ");
+            complaintType = sc.nextInt();
+            sc.nextLine(); // Consume newline left-over
+
+            if (complaintType != 0) {
+                inputComplaintStaff();
+            }
+        }while(complaintType!=0);
+    }
+
+    public void inputComplaintStaff() {
+        try {
+            System.out.println("Please enter your Complaint: ");
+            complaintText = sc.nextLine();
+            writeComplaintToFile(complaintType, complaintText);
+            System.out.println("Customer message saved to file.");
+            System.out.println("");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
+
+    public void promptComplaintCust() { //call this when run
         System.out.println("Select complaint type: " + "\n[1] Room Issue\n[2] Facilities\n[3] House keeping\n[4] Billing\n[5] Food\n[6] Service\n[0] Exit");
         System.out.print("Please choose an option: ");
         int complaintType = sc.nextInt();
         sc.nextLine(); // Consume newline left-over
 
         if (complaintType != 0) {
-            inputComplaint(complaintType);
+            inputComplaintCust(complaintType);
         }
     }
 
-    public void inputComplaint(int complaintType) {
+    public void inputComplaintCust(int complaintType) {
         try {
             System.out.print("Please enter your Complaint: ");
             String complaintText = sc.nextLine();
             writeComplaintToFile(complaintType, complaintText);
-            System.out.println("[\"We apologize and will do better. Thank you for your stay and feedback.\"]");
+            System.out.println("[We apologize and will do better. Thank you for your stay and feedback.]");
             System.exit(0);
         } catch (IOException e) {
             System.out.println("An error occurred while writing to the file.");
@@ -83,13 +108,7 @@ public class FeedbackSupport {
         }
 
         try (FileWriter writer = new FileWriter(fileName, true)) {
-            writer.write(complaintText + "\n---------------------------------------------------------");
+            writer.write(complaintText + "\n---------------------------------------------------------+\n");
         }
     }
 }
-
-
-
-
-
-
